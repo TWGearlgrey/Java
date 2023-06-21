bfb219b28e1006146086bb810231d9c33427970f
shipdesignup <shipdesignup@gmail.com>
2023-06-21 17:50:48 +0900

    백준 3-9부터

--- CodingTest/src/step3/Test09.java
+++ CodingTest/src/step3/Test09.java
@@ -1,6 +1,16 @@
 package step3;
 
+import java.util.Scanner;
+
+/*
+ * 날짜 : 2023/06/21
+ * 이름 : 한상민
+ * 내용 : 2438번. 별 찍기 - 1
+ */
 public class Test09 {
 	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		int N = 
 	}
 }
