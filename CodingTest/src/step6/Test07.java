b42b37b08d1889e3363df18289587b416122a081
shipdesignup <shipdesignup@gmail.com>
2023-07-05 17:08:45 +0900

    백준 2차원배열

--- CodingTest/src/step6/Test07.java
+++ CodingTest/src/step6/Test07.java
@@ -3,7 +3,7 @@ package step6;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2023/07/04
+ * 날짜 : 2023/07/05
  * 이름 : 한상민
  * 내용 : 1316. 그룹 단어 체커
  */
@@ -15,35 +15,39 @@ public class Test07 {
 		int n = in.nextInt();
 		
 		// 그룹단어 카운트 생성
-		int count;
+		int count = n;
 
 		// n만큼 문자열 입력
 		for(int i=0 ; i<n ; i++) {
 			String str = in.next();
-			for(int j=0 ; j<str.length() ; j++) {
-				int start = str.indexOf(String.valueOf(str.charAt(j)));
-				int end   = str.lastIndexOf(String.valueOf(str.charAt(j)));
-				
-				String a = str.substring(start, end+1);
-				String b = String.valueOf(str.charAt(j));
-				
-				for(int k=0 ; k<a.length() ; k++) {
-					b = b.concat(String.valueOf(str.charAt(j)));
-				}
-				
-				boolean result = b.equals(a);
-				
-			}
+			int len = str.length();
 			
+		/** 
+		 *  문자열의 인덱스 j에 있는 첫 인덱스 start와 
+		 * 그 문자가 마지막에 등장하는 인덱스 end를 구하여
+		 * 인덱스 start와 end 사이에서 값을 비교.
+		 *  
+		 *  다른 값이 나올시 안쪽에 있는 반복문을 중단하고 카운트.
+		 * 그 후 다시 문자열을 입력받아 검사한다.
+		 */
+			// 입력받은 문자열에서 확인할 문자열의 구간을 구함
+			outer: for(int j=0 ; j<len ; j++) {
+				int start = str.indexOf(String.valueOf(str.charAt(j)));
+				int end = str.lastIndexOf(String.valueOf(str.charAt(j)));
+				
+				// 그 구간에서 문자열 비교하여 카운트
+				for(int k=start ; k<=end ; k++) {
+					if (String.valueOf(str.charAt(k))
+							.equals(String.valueOf(str.charAt(end)))) {
+						continue;
+					} else {
+						count--;
+						break outer;
+					}
+				}
+			}
 		}
-	
-		// 중복되는 글자수를 파악 후 그 글자수만큼 연속된 수가 있는지 확인하기.
-		// substring(s, e)과 indexOf("e") lastIndexOf("e") 사용하기
-	
-	
-	
-	
-	
+		System.out.println(count);
 	
 		in.close();
 	}
