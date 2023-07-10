d3a7d042216097d612cef098d5e3799ac92e75dd
shipdesignup <shipdesignup@gmail.com>
2023-07-10 17:49:07 +0900

    백준 8의 6까지

--- CodingTest/src/step8/Test02.java
+++ CodingTest/src/step8/Test02.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2023/07/07
+ * 날짜 : 2023/07/10
  * 이름 : 한상민
  * 내용 : 11005. 진법 변환2
  */
@@ -13,29 +13,25 @@ public class Test02 {
 		
 		// 1. 숫자 n(10)과 b진수 b 입력
 		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		int b = in.nextInt();
+		int n = in.nextInt(); // 60466175
+		int b = in.nextInt(); // 36
 		
-		int mok = n; 									 // 몫은 n/b .. 마지막에 남은것만 저장
+		int mok = n;
 		ArrayList<Character> naMo = new ArrayList<>();	 // 나머지는 n%b .. 계속 저장해둬야함
 		
-		// 2. 몫은 덮어씌우고, 나머지는 바로 if문 넣어서 변경 후 char화
-		while(true) {
-			if(mok < b) { 		// 몫이 b보다 작으면 멈춤 (b는 b진수의 b) 
-				break;
-				
-			}else if(10 > mok%b) { // 나머지가 1~9인 숫자
-					mok = mok / b; 
-					naMo.add(Character.valueOf((char) (mok%b + 48)));
+		// 2. 몫은 덮어씌우고, 나머지는 List에 차례대로 넣기
+		while( mok > 0 ) {
 			
-			}else if(10 <= mok%b ) { // 나머지가 10~35인 숫자 (A to Z) 
-					mok = mok / b;
-					naMo.add(Character.valueOf((char) (mok%b + 64)));
+			if(mok%b < 10) {
+				naMo.add((char) (mok%b + '0'));
+			}else { 
+				naMo.add((char) (mok%b -10 + 'A'));
 			}
+			
+			mok /= b;
 		}
 		
-		// 3. 마지막 몫과 나머지 불러오기
-		System.out.print(mok);
+		// 3. 리스트 거꾸로 불러오기
 		for(int i=naMo.size()-1 ; i>=0 ; i--) {
 			System.out.print(naMo.get(i));
 		}
--- CodingTest/src/step8/Test06.java
+++ CodingTest/src/step8/Test06.java
@@ -1,12 +1,30 @@
 package step8;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /*
- * 날짜 : 2023/00/00
+ * 날짜 : 2023/07/10
  * 이름 : 한상민
- * 내용 : 0000. 제목
+ * 내용 : 1193. 분수찾기
  */
 public class Test06 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		
+		// 1~n개 리스트 생성
+		// 하나는 오름차순, 다른 하나는 내림차순 출력
+		// 카운트하여 순서 확인
+		int count = 0;
+		int line = 1;
+		ArrayList<Integer> nums = new ArrayList<>();
+		
+		while(count != x) {
+			
+		}	
+			
+		
 		
 	}
 }
