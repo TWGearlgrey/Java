9e42b08632faf0d7f4f479f5f57d73e09130bb2d
shipdesignup <shipdesignup@gmail.com>
2023-07-10 23:33:40 +0900

    백준9단계 돌입

--- CodingTest/src/step09/Test08.java
+++ CodingTest/src/step09/Test08.java
@@ -1,9 +1,9 @@
-package step8;
+package step09;
 
 /*
- * 날짜 : 2023/00/00
+ * 날짜 : 2023/07/10
  * 이름 : 한상민
- * 내용 : 0000. 제목
+ * 내용 : 5086. 배수와 약수
  */
 public class Test08 {
 	public static void main(String[] args) {
--- CodingTest/src/step8/Test06.java
+++ CodingTest/src/step8/Test06.java
@@ -1,6 +1,5 @@
 package step8;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /*
@@ -13,18 +12,32 @@ public class Test06 {
 		Scanner sc = new Scanner(System.in);
 		int x = sc.nextInt();
 		
-		// 1~n개 리스트 생성
-		// 하나는 오름차순, 다른 하나는 내림차순 출력
-		// 카운트하여 순서 확인
+		// x번째 숫자가 속한 줄 찾기
 		int count = 0;
-		int line = 1;
-		ArrayList<Integer> nums = new ArrayList<>();
+		int line = 0;
 		
-		while(count != x) {
-			
-		}	
-			
+		while(count < x) {
+			for(int i=0 ; i<=line ; i++) {
+				count++;
+			}
+			line++;
+		}
 		
+		// line 줄에 있는 숫자가 담긴 배열 생성
+		int[] arr = new int[line];
+		for(int i=0 ; i<line ; i++) {
+			arr[i] = i+1;
+		}
 		
+		// x번째 숫자 출력하기 (짝수줄은 분모내림차순, 홀수줄은 분모오름차순)
+		int n = x - (count - line) -1;
+		
+		if(line%2 == 0) {
+			System.out.println(arr[n]+"/"+arr[line-n-1]);
+		} else {
+			System.out.println(arr[line-n-1]+"/"+arr[n]);
+		}
+		
+		sc.close();
 	}
 }
