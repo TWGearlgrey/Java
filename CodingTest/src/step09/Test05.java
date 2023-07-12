8780862557ad6f02812bf75c10c34de0f5295217
shipdesignup <shipdesignup@gmail.com>
2023-07-12 13:48:48 +0900

    백준

--- CodingTest/src/step09/Test05.java
+++ CodingTest/src/step09/Test05.java
@@ -13,22 +13,43 @@ public class Test05 {
 		Scanner in = new Scanner(System.in);
 		int m = in.nextInt();
 		int n = in.nextInt();
-		
+		/**
 		ArrayList<Integer> arr = new ArrayList<>();
+		arr.add(0);
 		
+		// m과 n 대소 비교 후 작은 수를 m, 큰수를 n에 담기
 		if(m > n) {
 			int temp = m;
 			m = n;
 			n = temp;
 		}
 		
-		for(int i=m ; i<n ; i++) {
+		// m부터 n까지의 소수를 리스트에 담기
+		for(int i=m ; i<=n ; i++) {
 			int cnt = 0;
 			for(int j=1 ; j<i ; j++) {
-				if(m%j == 0) {
-					
+				if(i%j == 0) {
+					cnt++;
 				}
 			}
+			if(cnt == 1) {
+				arr.add(i);
+			}
 		}
+		
+		// 최소값과 총 합 출력
+		n = 0;
+		if (arr.size() > 0) {
+			for(int i=1 ; i<arr.size() ; i++) {
+				n += arr.get(i);
+			}
+			System.out.println(n);
+			System.out.print(arr.get(0));
+		} else {
+			System.out.print(-1);
+		}
+
+		in.close();
+		*/
 	}
 }
