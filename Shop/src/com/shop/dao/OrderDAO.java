9a8ca0afb9a73526c5092b1235d4862aa9ffcfe7
shipdesignup <shipdesignup@gmail.com>
2023-07-14 17:52:40 +0900

    전체백업

--- Shop/src/com/shop/dao/OrderDAO.java
+++ Shop/src/com/shop/dao/OrderDAO.java
@@ -1,8 +1,10 @@
 package com.shop.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.shop.db.DBHelper;
+import com.shop.db.SQL;
 import com.shop.vo.OrderVO;
 
 public class OrderDAO extends DBHelper {
@@ -14,16 +16,64 @@ public class OrderDAO extends DBHelper {
 	}
 	
 	// 기본 CRUD 메서드
-	public int insertOrder(OrderDAO vo) {
-		return 0;
+	public int insertOrder(OrderVO vo) {
+		
+		int result = 0;
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.INSERT_ORDER);
+			psmt.setString(1, vo.getOrderId());
+			psmt.setInt(2, vo.getOrderProduct());
+			psmt.setInt(3, vo.getOrderCount());
+			
+			result = psmt.executeUpdate();
+			
+			close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
 	}
 	
 	public OrderDAO selectOrder(String orderId) {
 		return null;
 	}
 	
-	public List<OrderDAO> selectOrders() {
-		return null;
+	public List<OrderVO> selectOrders(String orderId) {
+		
+		List<OrderVO> orders = new ArrayList<>();
+		
+		try {
+		
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.SELECT_ORDERS);
+			psmt.setString(1, orderId);
+			
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) {
+				OrderVO vo = new OrderVO();
+				vo.setOrderNo(rs.getInt(1));
+				vo.setOrderId(rs.getString(2));
+				vo.setOrderProduct(rs.getInt(3));
+				vo.setOrderCount(rs.getInt(4));
+				vo.setOrderDate(rs.getString(5).substring(1, 16)); //substr로 연월일 시분 까지 나타냄
+				vo.setName(rs.getString(6));
+				vo.setProdName(rs.getString(7));
+				
+				orders.add(vo);
+			}
+			
+			close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return orders;
 	}
 	
 	public int updateOrder(OrderVO vo) {
--- Shop/src/com/shop/dao/ProductDAO.java
+++ Shop/src/com/shop/dao/ProductDAO.java
@@ -1,8 +1,13 @@
 package com.shop.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.shop.db.DBHelper;
+import com.shop.db.SQL;
+import com.shop.vo.ProductVO;
 
 public class ProductDAO extends DBHelper {
 
@@ -18,16 +23,80 @@ public class ProductDAO extends DBHelper {
 		return 0;
 	}
 	
-	public ProductDAO selectProduct(int prodNo) {
-		return null;
+	public ProductVO selectProduct(int prodNo) {
+		
+		ProductVO vo = null;
+		
+		try {
+			Connection conn = getConnection();
+			PreparedStatement psmt = conn.prepareStatement(SQL.SELECT_PRODUCT);
+			psmt.setInt(1, prodNo);
+			
+			rs = psmt.executeQuery();
+			
+			if(rs.next()) {
+				vo = new ProductVO();
+				vo.setProdNo(rs.getInt("prodNo"));
+				vo.setProdName(rs.getString("prodName"));
+				vo.setStock(rs.getInt("stock"));
+				vo.setPrice(rs.getInt("price"));
+				vo.setCompany(rs.getString("company"));
+			}
+			
+			close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return vo;
 	}
 	
-	public List<ProductDAO> selectProducts() {
-		return null;
+	public List<ProductVO> selectProducts() {
+		
+		List<ProductVO> products = new ArrayList<>();
+		
+		try {
+			Connection conn = getConnection();
+			stmt = conn.createStatement();
+			rs  = stmt.executeQuery(SQL.SELECT_PRODUCTS);
+			
+			while(rs.next()) {
+				ProductVO vo = new ProductVO();
+				vo.setProdNo(rs.getInt("prodNo"));
+				vo.setProdName(rs.getString("prodName"));
+				vo.setStock(rs.getInt("stock"));
+				vo.setPrice(rs.getInt("price"));
+				vo.setCompany(rs.getString("company"));
+				
+				products.add(vo);
+			}
+			
+			close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return products;
 	}
 	
 	public int updateProduct() {
 		return 0;
+	}
+	
+	public void updateProductStock(int prodNo, int prodCount) {
+		
+		try {
+			conn = getConnection();
+			psmt = conn.prepareStatement(SQL.UPDATE_PRODUCT_STOCK);
+			psmt.setInt(1,  prodCount);
+			psmt.setInt(2,  prodNo);
+			
+			psmt.executeUpdate();
+			close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public int deleteProduct(int prodNo) {
