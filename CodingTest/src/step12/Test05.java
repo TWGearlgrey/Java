8faaa079705d88e021c534ecbf1984f908def25e
shipdesignup <shipdesignup@gmail.com>
2023-07-27 14:45:02 +0900

    전체 백업

--- CodingTest/src/step12/Test05.java
+++ CodingTest/src/step12/Test05.java
@@ -1,5 +1,7 @@
 package step12;
 
+import java.util.Scanner;
+
 /*
  * 날짜 : 2023/00/00
  * 이름 : 한상민
@@ -8,5 +10,26 @@ package step12;
 public class Test05 {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		String series = "666";
+		int fNum = 0; // 
+		int lNum = 0; // len-
+		
+		// lastNo.이 fNum >= 7 || fNum <= 5  ~> fNum++;, lstNo.=6일때 초기화
+		// lstNo.=6이 되는 순간, cnt를 fNum에, 0~9까지 lNum(lNum++;)
+		// 
+		
+	}
+	
+	public static int nt(String num) {
+		int result = Integer.parseInt(num);
+		return result;
+	}
+	
+	public static String st(int num) {
+		String result = String.valueOf(num);
+		return result;
 	}
 }
