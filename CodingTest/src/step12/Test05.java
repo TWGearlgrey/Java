48448b36369f2fe1f3ec07ab16db1d2df45cd314
shipdesignup <shipdesignup@gmail.com>
2023-07-30 21:46:34 +0900

    백준

--- CodingTest/src/step12/Test05.java
+++ CodingTest/src/step12/Test05.java
@@ -3,55 +3,32 @@ package step12;
 import java.util.Scanner;
 
 /*
- * 날짜 : 2023/00/00
+ * 날짜 : 2023/07/30
  * 이름 : 한상민
- * 내용 : 0000. 제목
+ * 내용 : 1436. 영화감독 숌
  */
 public class Test05 {
+	// int -> String 변환 메서드
+	public static String st(int num) {
+		return String.valueOf(num);
+	}
+	
+	// 메인
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		sc.close(); // 입력이 끝났으니 바로 닫아 줌
 		
-		String series = "666";
-		String result = null; 
-		int fNum = 0; 
-		int lNum = 0;
+		int srs = 666;
+		int cnt = 1;
 		
-		boolean switcH = true;
-		
-		for(int i=0 ; i<n ; i++) {
-			
-			// 앞자리가 1씩 증가하는 패턴
-			// 앞자리는 cnt고정, 뒷자리는 0부터 9까지 증가하는 패턴
-			// 앞자리가 66이 됐을 때 66600, 166600과 같은 경우
+		while(cnt < n) {
+			srs++;
+			if(st(srs).contains("666")) {
+				cnt++;
+			}
 		}
-		
-		System.out.println(result);
-		
-	}
-	
-	// String -> int 변환
-	public static int nt(String num) {
-		int result = Integer.parseInt(num);
-		return result;
-	}
-	
-	// int -> String 변환
-	public static String st(int num) {
-		String result = String.valueOf(num);
-		return result;
-	}
-	
-	// 숫자 끝자리 추출하기
-	public static int last(int num) {
-		int result = st(num).charAt((st(num).length()));
-		return result;
-	}
-	
-	// 숫자 첫자리 추출하기
-	public static int first(int num) {
-		int result = st(num).charAt(0);
-		return result;
+		System.out.println(srs);
 	}
 }
